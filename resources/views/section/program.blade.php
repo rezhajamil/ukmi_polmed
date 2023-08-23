058147a9525d77bca99c85cbb3294e923eafe5ef
John Doe <yamatogija@gmail.com>
2023-08-23 11:52:54 +0700

    Add Program

--- resources/views/section/program.blade.php
+++ resources/views/section/program.blade.php
@@ -2,7 +2,7 @@
     <h1 class="mb-4 text-3xl font-bold text-center text-white xl:text-5xl">
         Di <span class="text-yellow-400">UKMI</span> ngapain aja sih??
     </h1>
-    <div class="grid grid-cols-1 gap-4 px-4 py-8 sm:grid-cols-2 sm:px-6">
+    <div class="grid grid-cols-1 px-4 py-8 gap-y-12 sm:px-6">
         <div class="flex flex-col gap-6">
             <span class="text-base font-bold text-center text-white sm:text-2xl">
                 Mentoring & Sharing Asyik??
@@ -15,8 +15,6 @@
                     class="sm:w-[600px] border-[12px] border-white rounded w-[300px] scale-90 h-[180px] sm:h-[350px] -rotate-6 object-cover object-center aspect-video">
                 <img src="{{ asset('images/mentoring-1.jpg') }}" alt="Mentoring UKMI"
                     class="sm:w-[600px] border-[12px] border-white rounded w-[300px] h-[180px] sm:h-[350px] rotate-[10deg] -mt-16 sm:-ml-12 -ml-14 object-cover object-center aspect-video">
-                <div class="">
-                </div>
             </div>
         </div>
         <div class="flex flex-col gap-6">
@@ -25,14 +23,14 @@
                 <span class="text-yellow-400"> Hayuk!!</span>
             </span>
             <div class="relative flex flex-col items-center sm:min-h-screen">
-                @include('components.bg-dot-teal', ['position' => '-right-8 top-16'])
-                @include('components.bg-dot-teal', ['position' => '-left-8 top-8'])
-                <img src="{{ asset('images/mentoring-3.jpg') }}" alt="Mentoring UKMI"
+                @include('components.bg-dot-teal', ['position' => '-right-8 bottom-16'])
+                @include('components.bg-dot', ['position' => '-left-8 top-8'])
+                <img src="{{ asset('images/camp-1.jpg') }}" alt="Camping UKMI"
                     class="sm:w-[600px] border-[12px] border-white rounded w-[300px] scale-90 h-[180px] sm:h-[350px] -rotate-6 object-cover object-center aspect-video">
-                <img src="{{ asset('images/mentoring-1.jpg') }}" alt="Mentoring UKMI"
-                    class="sm:w-[600px] border-[12px] border-white rounded w-[300px] h-[180px] sm:h-[350px] rotate-[10deg] -mt-16 sm:-ml-12 -ml-14 object-cover object-center aspect-video">
-                <div class="">
-                </div>
+                <iframe src="https://drive.google.com/file/d/1zzEre0lPne9Sd0j7OO46VyuijHP40DjF/preview"
+                    class="sm:w-full border-[12px] border-white rounded w-full h-full sm:h-[550px] rotate-[2deg] -mt-4 sm:-mr-6 -mr-8 object-cover object-center aspect-video"
+                    width="640" height="480" allow="autoplay">
+                </iframe>
             </div>
         </div>
     </div>
