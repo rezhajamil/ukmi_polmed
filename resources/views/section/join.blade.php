3f3b46b8c7c732e047e0eea7bfb3010ba1912154
John Doe <yamatogija@gmail.com>
2023-08-26 13:24:24 +0700

    Add Join

--- resources/views/section/join.blade.php
+++ resources/views/section/join.blade.php
@@ -2,84 +2,84 @@
     <h1 class="mb-4 text-3xl font-bold text-center text-white xl:text-6xl">
         Menarik kan?? Ayo buruan gabung <span class="text-yellow-400">UKMI</span>
     </h1>
-    <div class="w-full py-4 grid grid-cols-2 sm:grid-cols-3 px-4 sm:px-8 gap-4">
-        <span class="col-span-full text-white font-semibold sm:text-lg ">Persyaratannya mudah kok..</span>
+    <div class="grid w-full grid-cols-2 gap-4 px-4 py-4 sm:grid-cols-3 sm:px-8">
+        <span class="font-semibold text-white col-span-full sm:text-lg ">Persyaratannya mudah kok..</span>
         <div class="col-span-1">
             <div
-                class="shadow-lg flex flex-col justify-center items-center rounded-lg p-4 text-white bg-gradient-to-tr sm:bg-gradient-to-t from-teal-600 to-teal-800 w-full h-40 sm:h-48">
+                class="flex flex-col items-center justify-center w-full h-40 p-4 text-white rounded-lg shadow-lg bg-gradient-to-tr sm:bg-gradient-to-t from-teal-600 to-teal-800 sm:h-48">
                 <span class="text-2xl font-bold">
                     1.
                 </span>
-                <p class="sm:text-xl text-center text-lg font-bold">
+                <p class="text-lg font-bold text-center sm:text-xl">
                     Mahasiswa POLMED
                 </p>
             </div>
         </div>
         <div class="col-span-1">
             <div
-                class="shadow-lg flex flex-col justify-center items-center rounded-lg p-4 text-white bg-gradient-to-tl sm:bg-gradient-to-t from-teal-600 to-teal-800 w-full h-40 sm:h-48">
+                class="flex flex-col items-center justify-center w-full h-40 p-4 text-white rounded-lg shadow-lg bg-gradient-to-tl sm:bg-gradient-to-t from-teal-600 to-teal-800 sm:h-48">
                 <span class="text-2xl font-bold">
                     2.
                 </span>
-                <p class="sm:text-xl text-center text-lg font-bold">
+                <p class="text-lg font-bold text-center sm:text-xl">
                     Beragama Islam
                 </p>
             </div>
         </div>
         <div class="col-span-full sm:col-span-1">
             <div
-                class="shadow-lg flex flex-col justify-center items-center rounded-lg p-4 text-white bg-gradient-to-b sm:bg-gradient-to-t from-teal-600 to-teal-800 sm:w-full w-3/4 mx-auto sm:mx-0 h-40 sm:h-48">
+                class="flex flex-col items-center justify-center w-3/4 h-40 p-4 mx-auto text-white rounded-lg shadow-lg bg-gradient-to-b sm:bg-gradient-to-t from-teal-600 to-teal-800 sm:w-full sm:mx-0 sm:h-48">
                 <span class="text-2xl font-bold">
                     3.
                 </span>
-                <p class="sm:text-xl text-center text-lg font-bold">
+                <p class="text-lg font-bold text-center sm:text-xl">
                     Bersedia mengikuti mentoring pekanan
                 </p>
             </div>
         </div>
     </div>
     <div class="w-full px-4">
-        <div class="w-full border-2 border-white rounded-lg px-3 py-4">
-            <span class="w-full inline-block text-center text-base sm:text-2xl text-white font-bold mb-4">
+        <div class="w-full px-3 py-4 border-2 border-white rounded-lg">
+            <span class="inline-block w-full mb-4 text-base font-bold text-center text-white sm:text-2xl">
                 FORMULIR PENDAFTARAN
             </span>
-            <form action="" method="GET" class="w-full grid grid-cols-2 gap-4" id="form-join" target="_blank">
+            <form action="" method="GET" class="grid w-full grid-cols-2 gap-4" id="form-join" target="_blank">
                 <div class="flex flex-col">
-                    <label for="nama" class=" text-white font-semibold">Nama Lengkap</label>
+                    <label for="nama" class="font-semibold text-white ">Nama Lengkap</label>
                     <input type="text" name="nama" id="nama"
-                        class="rounded-md focus:ring-yellow-400 border-white border-2 bg-transparent focus:border-yellow-400 text-white">
+                        class="text-white bg-transparent border-2 border-white rounded-md focus:ring-yellow-400 focus:border-yellow-400">
                 </div>
                 <div class="flex flex-col">
-                    <label for="whatsapp" class=" text-white font-semibold">Nomor Whatsapp</label>
+                    <label for="whatsapp" class="font-semibold text-white ">Nomor Whatsapp</label>
                     <input type="number" name="whatsapp" id="whatsapp"
-                        class="rounded-md focus:ring-yellow-400 border-white border-2 bg-transparent focus:border-yellow-400 text-white">
+                        class="text-white bg-transparent border-2 border-white rounded-md focus:ring-yellow-400 focus:border-yellow-400">
                 </div>
                 <div class="flex flex-col">
-                    <label for="jenis_kelamin" class=" text-white font-semibold">Jenis Kelamin</label>
+                    <label for="jenis_kelamin" class="font-semibold text-white ">Jenis Kelamin</label>
                     <select name="jenis_kelamin" id="jenis_kelamin"
-                        class="rounded-md focus:ring-yellow-400 border-white border-2 bg-transparent focus:border-yellow-400 text-white">
+                        class="text-white bg-transparent border-2 border-white rounded-md focus:ring-yellow-400 focus:border-yellow-400">
                         <option class="text-black" value="" selected disabled>Pilih Jenis Kelamin</option>
                         <option class="text-black" value="laki-laki">Laki-Laki</option>
                         <option class="text-black" value="perempuan">Perempuan</option>
                     </select>
                 </div>
                 <div class="flex flex-col">
-                    <label for="alamat" class=" text-white font-semibold">Alamat</label>
+                    <label for="alamat" class="font-semibold text-white ">Alamat</label>
                     <input type="text" name="alamat" id="alamat"
-                        class="rounded-md focus:ring-yellow-400 border-white border-2 bg-transparent focus:border-yellow-400 text-white">
+                        class="text-white bg-transparent border-2 border-white rounded-md focus:ring-yellow-400 focus:border-yellow-400">
                 </div>
                 <div class="flex flex-col">
-                    <label for="prodi" class=" text-white font-semibold">Program Studi</label>
+                    <label for="prodi" class="font-semibold text-white ">Program Studi</label>
                     <input type="text" name="prodi" id="prodi"
-                        class="rounded-md focus:ring-yellow-400 border-white border-2 bg-transparent focus:border-yellow-400 text-white">
+                        class="text-white bg-transparent border-2 border-white rounded-md focus:ring-yellow-400 focus:border-yellow-400">
                 </div>
                 <div class="flex flex-col">
-                    <label for="kelas" class=" text-white font-semibold">Kelas</label>
+                    <label for="kelas" class="font-semibold text-white ">Kelas</label>
                     <input type="text" name="kelas" id="kelas"
-                        class="rounded-md focus:ring-yellow-400 border-white border-2 bg-transparent focus:border-yellow-400 text-white">
+                        class="text-white bg-transparent border-2 border-white rounded-md focus:ring-yellow-400 focus:border-yellow-400">
                 </div>
                 <button type="submit"
-                    class="rounded-md w-full sm:w-10/12 mx-auto bg-yellow-400 hover:bg-yellow-500 transition-all text-center cursor-pointer col-span-full font-bold text-emerald-800 px-4 py-3">
+                    class="w-full px-4 py-3 mx-auto font-bold text-center transition-all bg-yellow-400 rounded-md cursor-pointer sm:w-10/12 hover:bg-yellow-500 col-span-full text-emerald-800">
                     GABUNG UKMI
                 </button>
             </form>
@@ -114,7 +114,7 @@
                 if (jenis_kelamin == 'laki-laki') {
                     url += "6282289138698";
                 } else if (jenis_kelamin == 'perempuan') {
-                    url += '6281269863028';
+                    url += '6281264119612';
                 }
 
                 console.log(url + "?text=" + message);
